180ad537a83734fd3b39127f74d3ea0303c5e916
Angheng <bungae1112@gmail.com>
2022-07-01 03:02:39 +0000

    init

---
+++ src/cam_info_publisher.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "sensor_msgs/CameraInfo.h"
+
+void setCamInfo(sensor_msgs::CameraInfo* tmp)
+{
+	tmp->height = 480;
+	tmp->width = 640;
+	tmp->distortion_model = "plumb_bob";
+	tmp->D = {0.0, 0.0, 0.0, 0.0, 0.0};
+	tmp->K = {384.08355712890625, 0.0, 321.8194885253906, 0.0, 384.08355712890625, 238.3449249267578, 0.0, 0.0, 1.0};
+	tmp->R = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
+	tmp->P = {384.08355712890625, 0.0, 321.8194885253906, 0.0, 0.0, 384.08355712890625, 238.3449249267578, 0.0, 0.0, 0.0, 1.0, 0.0};
+	tmp->binning_x = 0;
+	tmp->binning_y = 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+	ros::init(argc, argv, "cam_info_publisher");
+	ros::NodeHandle node;
+	ros::Rate rate(10);
+	
+	std::string name;
+	node.getParam("/cam_info_publisher/name", name);
+	ROS_WARN("Running %s's Camera info publisher", name.c_str());
+
+	ros::Publisher sender_cam_info = node.advertise<sensor_msgs::CameraInfo> (name+ "/camera_info", 1000);
+	
+	sensor_msgs::CameraInfo cam_info;
+	cam_info.header.frame_id = name + "_frame";
+	setCamInfo(&cam_info);
+
+    while(ros::ok())
+    {
+		cam_info.header.stamp = ros::Time::now();
+		sender_cam_info.publish(cam_info);
+
+		rate.sleep();
+    }
+
+    return 0;
+}	
